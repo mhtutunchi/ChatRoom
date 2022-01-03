fa5649bf2fe7139bb206c1b4f9b3e03b30ef931c
mhtutunchi <9625216Mm>
2022-01-03 17:56:33 +0330

    Final

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -17,7 +17,7 @@ public class ClientHandler implements Runnable {
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.clientUsername = bufferedReader.readLine();
             clientHandlers.add(this);
-            broadcastMessage("SERVER: " + clientUsername + "has Entered the chat!");
+            broadcastMessage("SERVER: " + clientUsername + " has Entered the chat!");
         } catch (IOException e) {
             closeEverything(socket, bufferedReader, bufferedWriter);
         }
@@ -56,7 +56,7 @@ public class ClientHandler implements Runnable {
 
     public void removeClientHandler() {
         clientHandlers.remove(this);
-        broadcastMessage("SERVER" + clientUsername + "has left the chat!");
+        broadcastMessage("SERVER" + clientUsername + " has left the chat!");
     }
 
     public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
